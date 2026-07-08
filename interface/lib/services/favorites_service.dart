342077e061a5df2b5c3aec485fd8961a013678a8
tiago-moroni6340 <d2023006340@unifei.edu.br>
2026-07-08 15:02:25 -0300

    Remove comments from FavoritesService class
    Removed comments explaining the FavoritesService functionality.

--- interface/lib/services/favorites_service.dart
+++ interface/lib/services/favorites_service.dart
@@ -1,9 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-/// Serviço de favoritos. A API do `openai.yaml` não possui nenhum endpoint
-/// de favoritos, então essa funcionalidade é implementada 100% no
-/// dispositivo (armazenamento local com SharedPreferences), guardando os
-/// IDs (`nba_player_id`) dos jogadores favoritados.
 class FavoritesService {
   static const _key = 'favorite_player_ids';
 
